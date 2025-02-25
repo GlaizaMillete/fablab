a2f1bfb84feaf8cd7e14279a532887e5ba9f371a
Glaiza <milletemea@gmail.com>
2025-02-25 10:09:40 +0800

    Initial commit

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>FabLab Master Portal</title>
+</head>
+<body>
+    <div>
+        <div class="login-card">
+            <h1>FabLab Master Portal</h1><br>
+            <div class="login-inputs">
+            <form action="" method="">
+                <input type="text" name="username" placeholder="Username">
+                <input type="password" name="password" placeholder="Password">
+                <input type="submit" name="login" class="login login-submit" value="Login">
+            </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
