c5cd5cb0e98809bbbe4df435521f41f5e48afead
Jade Raposa <jaduya678@gmail.com>
2025-02-27 01:47:21 +0800

    committing so it looks like I made progress

--- home.php
+++ home.php
@@ -1,23 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <title>FabLab Master Portal</title>
 </head>
+
 <body>
     <div>
         <div class="login-card">
-            <h1>FabLab Master Portal</h1><br>
+            <i class="fas fa-user-circle"></i>
+            <h1>FABLAB Master Portal</h1><br>
             <div class="login-inputs">
-            <form action="" method="">
-                <input type="text" name="username" placeholder="Username">
-                <input type="password" name="password" placeholder="Password">
-                <input type="submit" name="login" class="login login-submit" value="Login">
-            </form>
+                <form action="" method="">
+                    <input type="text" name="username" placeholder="Username">
+                    <input type="password" name="password" placeholder="Password">
+                    <input type="submit" name="login" class="login login-submit" value="Login">
+                </form>
             </div>
         </div>
     </div>
 </body>
+
 </html>
