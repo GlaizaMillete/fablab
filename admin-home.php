ed230e203e26625e6d9a84eb5ab6d2fc2ebc0b10
Glaiza <milletemea@gmail.com>
2025-03-04 15:53:31 +0800

    admin home page (lacks content code)

---
+++ admin-home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Admin Home</title>
+</head>
+
+<body>
+    <div class="admin-container">
+        <div class="admin-left">
+            <div class="admin-header">
+                <img src="path/to/your/image.jpg" alt="Description of image" class="admin-image">
+
+            </div>
+            <div class="admin-content">
+                <div>
+                    <p>Job Requests</p>
+                </div>
+            </div>
+        </div>
+        <div class="admin-right">
+            <div class="full-width-container">
+                <div class="add-button-container">
+                    <p>Add</p>
+                </div>
+            </div>
+            <div class="contents">
+                <div class="contents-box">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
